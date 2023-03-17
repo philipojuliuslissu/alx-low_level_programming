5eaa0abede41d19f4a60f8a4126fed0bc227f19b
philipojuliuslissu <philiplissu3@gmail.com>
2023-03-17 05:50:09 -0700

    Second C project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n;
+    srand(time(NULL));
+    n = rand() % 201 - 100; // random number between -100 and 100 inclusive
+    printf("The number %d ", n);
+    if (n > 0) {
+        printf("is positive\n");
+    } else if (n == 0) {
+        printf("is zero\n");
+    } else {
+        printf("is negative\n");
+    }
+    return 0;
+}
+
